2e2369e8109ef99602746fd126f69ff73a0569b2
clemence laurent <clemencelaurent7@gmail.com>
2022-04-24 19:50:12 +0200

    ajout du create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,10 +12,13 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  def edit
-  end
-
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def updated
@@ -24,4 +27,9 @@ class ListsController < ApplicationController
   def destroy
   end
 
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
